eedd67ce760cce7414604e029a30722155764b20
Smiggy <70798673+Smigg-y@users.noreply.github.com>
2024-12-23 22:31:35 +0000

    add shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -14,4 +14,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   </React.StrictMode>,
 )
 
-postMessage({ payload: 'removeLoading' }, '*')
+postMessage({ payload: "removeLoading" }, "*");
